7aa34da286ce20ff839b450adfd4c06183629220
henrytheyang <contacthenryyang@gmail.com>
2018-07-20 16:17:16 -0700

    Complete naive solution

--- romanToInteger.js
+++ romanToInteger.js
@@ -1,2 +1,28 @@
 // Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
 
+var romanToInt = function(s) {
+  var runningTotal = 0;
+  const conversionFactor = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let currentIndex = 0;
+  while (currentIndex < s.length - 1) {
+//     debugger
+    let currentLetter = s[currentIndex];
+    let nextLetter = s[currentIndex + 1];
+    if (conversionFactor[currentLetter] >= conversionFactor[nextLetter]) {
+      runningTotal += conversionFactor[currentLetter];
+    } else {
+      runningTotal -= conversionFactor[currentLetter];
+    }
+    currentIndex += 1;
+  }
+  runningTotal += conversionFactor[s[s.length - 1]];
+  return runningTotal;
+};
