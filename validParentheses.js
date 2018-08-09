cb159dc96f90b01f259143c40cacca5862dedd24
henrytheyang <contacthenryyang@gmail.com>
2018-08-09 16:06:58 -0700

    Complete solution- can optimize in future

--- validParentheses.js
+++ validParentheses.js
@@ -14,8 +14,8 @@
 var isValid = function(s) {
   // Use stack to store input string history, first in first out
   // If we encounter closed parens without a corresponding open parens at the top of the stack, return false
+  // If we encounter a closed parens that matches the last item on the stack (an open parens), we remove the last item on the stack
   // As we encounter open parens, we add them to the stack.
-  // If we encounter a closd parens that matches the last item on the stack (an open parens), we remove the last item on the stack
   // If we get to the end of the string and the stack is empty return true
   // Else return false
   // Note- because the input string is only parens, we could do this more naively but we'll make something more robust and useful
@@ -25,6 +25,11 @@ var isValid = function(s) {
     '[': ']',
     '{': '}',
   };
+  let closingParens = {
+    ')': true,
+    ']': true,
+    '}': true,
+  };
   let parensStack = [];
   if (s.length === 0) {
     return true;
@@ -33,18 +38,35 @@ var isValid = function(s) {
   let currentLetter = '';
   for (var i = 0; i < s.length; i++) {
     currentLetter = s[i];
-    // Check for closing parens first error
-    for (var key in parensPairs) {
-      if (parensPairs[key] === currentLetter) {
-        // validity = false;
-        // break;
-        return false; //TEST THIS HERE
-      }
+    // If currentLetter is a closing parens and it doesn't match the top of the stack string is invalid
+    if (closingParens[currentLetter] === true && parensPairs[parensStack[parensStack.length - 1]] !== currentLetter) {
+      validity = false;
+      // console.log('false catcher catching yay');
+      break;
+    }
+    // If currentLetter is a closing parens and it DOES match the top of the stack, destack parensStack
+    if (parensPairs[parensStack[parensStack.length - 1]] === currentLetter) {
+      parensStack.pop();
+      // console.log('destacked parensStack');
     }
     // Push first open parens onto stack
     if (parensPairs[currentLetter]) {
       parensStack.push(currentLetter);
+      // console.log('pushed an open parens onto stack');
     }
   }
+  // After iterating through string check for leftover unmatched parens
+  if (parensStack.length > 0) {
+    validity = false;
+  }
+  console.log('s = ', s, ' validity = ', validity);
+  // console.log('parensStack = ', parensStack);
   return validity;
 };
+
+isValid('()');
+isValid('()[]{}');
+isValid('(]');
+isValid('([)]');
+isValid('{[]}');
+isValid('a()(');
