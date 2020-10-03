6f9f9eee562e318170829ae2038bf229464043e1
Henry Yang <henrytheyang@Henrys-MBP.attlocal.net>
2020-10-03 13:58:05 -0700

    Add pseudocode

--- longestSubstringWithoutRepeats.js
+++ longestSubstringWithoutRepeats.js
@@ -33,5 +33,13 @@ s consists of English letters, digits, symbols and spaces.
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    
+  // Loop through every index position. Find longest substring from each starting point
+    // Loop through subsequent letters to build longest substring
+    // Store encountered characters in an object
+    // Check each letter to see if it's in the letter bank
+      // If it's missing, add it to the letter bank and update longest substring
+      // If it's present, stop the interior loop.
+        // Compare present substring to stored longest substring.
+          // If new substring is longer, store it as longest substring
+    // Check if we've reached prohibitive distance from end
 };
