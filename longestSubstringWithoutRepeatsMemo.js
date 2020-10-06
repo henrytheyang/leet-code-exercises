2a8c1ff7a701bc982037ea9453bc58e9b832516b
henrytheyang <contacthenryyang@gmail.com>
2020-10-06 11:12:56 -0700

    Memo version slightly slower for shorter strings without restarts but uses slightly less memory and more efficient with restarts

--- longestSubstringWithoutRepeatsMemo.js
+++ longestSubstringWithoutRepeatsMemo.js
@@ -36,6 +36,9 @@ var lengthOfLongestSubstring = function(s) {
   let charBank = {};
   let answer = '';
   let tempAnswer = '';
+  let substringBank = [];
+  let sliceOfTempAnswer = '';
+  // example substringBank entry: {sub:'abc'};
   if (s === '') {
     return 0;
   }
@@ -44,8 +47,16 @@ var lengthOfLongestSubstring = function(s) {
     // As we add strings to answer string while on current i, add string results to 
     // following indices.
     // When starting the loop at the next index i, start further down using the saved substring
-
   for (i = 0; i <= s.length - answer.length; i++) {
+    // Check bank for existing substring, then update tempAnswer and advance i beyond last index to test;
+    // Skip all s[i] scans that have been memoized
+    if (substringBank[i] !== undefined) {
+      continue;
+    }
+    // Retrieve memoized data, if any
+    if (substringBank[i] !== undefined) {
+      tempAnswer = substringBank[i];
+    } 
     for (j = i; j <= s.length - 1; j++) {
       if (charBank[s[j]] === undefined) {
         charBank[s[j]] = true;
@@ -54,16 +65,28 @@ var lengthOfLongestSubstring = function(s) {
           answer = tempAnswer;
         }
       } else {
-        charBank = {};
-        tempAnswer = '';
+        // When we run into a repeat, update the subStringBank data for all indices running the length of the current substring
+        for (k = i; k <= i + tempAnswer.length - 1; k++) {
+          // abca
+          // 0123
+          // Lop off chars from the beginning of string and update substringBank[k] with remainder if it's longer than current substring[k];
+          sliceOfTempAnswer = tempAnswer.slice(k);
+          if (sliceOfTempAnswer.length > substringBank[k]) {
+            substringBank[k] = sliceOfTempAnswer;
+          }
+          charBank = {};
+          tempAnswer = '';
+        }
         break;
       }
     }
   }
+  console.log(`answer = ${answer}`);
   return answer.length;
 };
 
-// lengthOfLongestSubstring("abcabcbb");
-// Output 2, expected 3
+// console.log(lengthOfLongestSubstring("abcabcbb"));
+// console.log(lengthOfLongestSubstring("bbbbb"));
 // console.log(lengthOfLongestSubstring("pwwkew"));
-console.log(lengthOfLongestSubstring("bbbbb"));
+//expected 6, got 5. Cut off the first t from answer
+console.log(lengthOfLongestSubstring('bbtablud'));
