ab218b748649518b3799d336c56a42544344c5cd
Henry Yang <contacthenryyang@gmail.com>
2020-10-08 11:40:50 -0700

    Finish first attempt at recursive solve

--- symmetricTree.js
+++ symmetricTree.js
@@ -45,16 +45,51 @@ Follow up: Solve it both recursively and iteratively.
 
 var isSymmetric = function(root) {
   // Scan node's children, add children to current queue
+  let valueBank = [];
+  let currentQueue = [];
+  let nextLevelQueue = [];
+  let answer = true;
+
+  if (root.left === null && root.right === null) {
+    return true;
+  }
+  currentQueue.push(root.left, root.right);
+
+  let internalRecursiveFx = (someQueue) => {
   // Recursively, on current queue:
     // Add current queue nodes values to value bank
     // Add children to next level queue
+    for (i = 0; i < someQueue.length - 1; i++) {
+      valueBank.push(someQueue[i] === null ? null : someQueue[i].val);
+      nextLevelQueue.push(someQueue[i].left, someQueue[i].right);
+    }
+
     // Check value bank for mirror status
       // If false, return false
+    // [0, 1, 1, 0], [0, 1, 2, 2, 1, 0]
+    //  0  1  2  3    0  1  2  3  4  5  
+    let arrayMid = (valueBank.length / 2) - 1;
+    for (j = 0; j <= arrayMid; j++) {
+      if (valueBank[j] !== valueBank[valueBank.length - 1 - j]) {
+        answer = false;
+        break;
+      }
+    };
+    if (answer === false) {
+      return answer;
+    }
+    
     // If there are nodes in the next level's queue
       // Transfer next level queue to current queue
       // Blank next level queue
       // Recurse
+    if (nextLevelQueue.length > 0) {
+      currentQueue = nextLevelQueue;
+      nextLevelQueue = [];
+      internalRecursiveFx(currentQueue);
+    }
+  };
 
-
-  return true;
+  internalRecursiveFx(currentQueue);
+  return answer;
 };
