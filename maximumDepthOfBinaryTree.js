ac25e7d24a1150dd4554ae93883ddccdcd6608eb
henrytheyang <contacthenryyang@gmail.com>
2020-10-13 09:37:12 -0700

    Create file and outline problem

--- maximumDepthOfBinaryTree.js
+++ maximumDepthOfBinaryTree.js
@@ -16,3 +16,19 @@ Given binary tree [3,9,20,null,null,15,7],
    15   7
 return its depth = 3.
 */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    
+};
