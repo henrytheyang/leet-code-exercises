2a8cb7e184347cd5586c8cea8bc8962b6b688e80
Henry Yang <contacthenryyang@gmail.com>
2020-10-15 15:02:23 -0700

    Modify node-creator and finish breadth-first search

--- maximumDepthOfBinaryTree.js
+++ maximumDepthOfBinaryTree.js
@@ -17,14 +17,15 @@ Given binary tree [3,9,20,null,null,15,7],
 return its depth = 3.
 */
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+  return {
+    val : (val===undefined ? 0 : val),
+    left : (left===undefined ? null : left),
+    right : (right===undefined ? null : right),
+  }
+}
+ 
 /**
  * @param {TreeNode} root
  * @return {number}
@@ -38,6 +39,7 @@ var maxDepth = function(root) {
   // Increment counter
   // Recurse on queue
   let internalRecursiveFx = (someQueue) => {
+    answer++;
     for (i = 0; i <= someQueue.length - 1; i++) {
       if (someQueue[i] !== null & someQueue[i].left !== null) {
         nextQueue.push(someQueue[i].left);
@@ -46,12 +48,11 @@ var maxDepth = function(root) {
         nextQueue.push(someQueue[i].right);
       }
     }
-    if (nextQueue.length > 0) {
-      answer++;
-    }
     childrenQueue = nextQueue;
     nextQueue = [];
-    internalRecursiveFx(childrenQueue);
+    if (childrenQueue.length > 0) {
+      internalRecursiveFx(childrenQueue);
+    }
   }
 
   if (root !== null) {
@@ -60,14 +61,25 @@ var maxDepth = function(root) {
     return 0;
   }
   // Scan root node for children, add to queue. Recurse on queue
-  if (root.left !== null) {
-    childrenQueue.push(root.left);
+  if (root !== null) {
+    if (root.left !== null) {
+      childrenQueue.push(root.left);
+    }
+    if (root.right !== null) {
+      childrenQueue.push(root.right);
   }
-  if (root.right !== null) {
-    childrenQueue.push(root.right);
   }
   if (childrenQueue.length > 0) {
     internalRecursiveFx(childrenQueue);
   }
+  console.log(`answer = ${answer}`);
   return answer;
 };
+
+let a = TreeNode(7, null, null);
+let b = TreeNode(15, null, null);
+let c = TreeNode(20, b, a);
+let d = TreeNode(9, null, null);
+let e = TreeNode(3, d, c);
+
+maxDepth(e);
