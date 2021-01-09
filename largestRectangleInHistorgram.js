b4d62b66712a9bbbb82776f271f69e323536bb77
Henry Yang <contacthenryyang@gmail.com>
2021-01-08 19:43:31 -0800

    Pseudocode non-brute force solution

--- largestRectangleInHistorgram.js
+++ largestRectangleInHistorgram.js
@@ -22,95 +22,97 @@ Output: 10
  * @param {number[]} heights
  * @return {number}
  */
+
 var largestRectangleArea = function(heights) {
-  // Find greatest rectangle at each index point, for all heights it covers
-  // Iterate through array's x-axis
-    // Iterate through current x-coord's y-axis
-    // Check to see if domain is memoized at this x,y
-    // If not:
-      // Memoize the domain at each height
-      // Iterate through array to see how far right a continuous rectangle extends at the current height
-      // Store the rectangle's domain
-    // If it is: 
-      // Recall rectangle width
-    // Determine largest rectangle that has this x,y at the top. Use smallest left,right that is in common for all heights at this x-coord
-    // Compare area to stored largest area
-      // If the area > stored area, store new largest area
-  // Return largest area
-
-  /*
-  storedDomains = [
-    [],
-    [[0, 2], [3, 5]],
-  ]
-  */
-
-  let largestArea = 0;
-  let storedDomains = [
-    [],
-  ];
-  const scanRightAtHeight = (someHeight, someStartIndex) => {
-    let domain = [];
-    for (let x = someStartIndex; x < heights.length; x++) {
-      if (someHeight <= heights[x]) {
-        if (domain[0] === undefined) {
-          domain = [x, x + 1];
-        } else {
-          domain[1] = x + 1;
-        }
-      } else {
-        // console.log(`domain = ${JSON.stringify(domain)}`)
-        return domain;
-      }
-    }
-    console.log(`domain = ${JSON.stringify(domain)}`)
-    return domain;
-  }
-
-  if (heights.length === 0) {
-    return 0;
-  }
-
-  for (let x = 0; x < heights.length; x++) {
-    let currentWidth = [];
-    let previousWidth = [];
-    let currentArea = 0;
-    let y = 1;
-    while (y <= heights[x]) {
-      // If data at height is stored
-      if (storedDomains[y]) {
-        // Check to see if stored data at current height contains data at x
-        for (let i = 0; i < storedDomains[y].length; i++) {
-          // If data at height exists and x is in a stored domain
-          if (storedDomains[y][i][0] <= x && storedDomains[y][i][1] >= x) {
-            currentWidth = [storedDomains[y][i][0], storedDomains[y][i][1]]
-          } else {
-            // If data at height doesn't contain x, it's in a new domain; store it
-            currentWidth = scanRightAtHeight(y, x);
-            storedDomains[y].push(currentWidth);
-          }
-        }
-      } else {
-        // If no domains stored at height
-        currentWidth = scanRightAtHeight(y, x);
-        storedDomains[y] = [currentWidth];
-      }
-      // Calculate rectangle area at current x, for each height
-      // If previousWidth empty
-      if (previousWidth[0] !== undefined) {
-        // If currentWidth[0] is left of previousWidth[0], replace
-        currentWidth[0] = (currentWidth[0] < previousWidth[0]) ? previousWidth[0] : currentWidth[0];
-        // If currentWidth[1] is right of previousWidth[1], replace
-        currentWidth[1] = (currentWidth[1] > previousWidth[1]) ? previousWidth[1] : currentWidth[1];        
-      }
-      currentArea = (currentWidth[1] - currentWidth[0]) * y;
-      largestArea = (largestArea > currentArea) ? largestArea : currentArea;
-      previousWidth = currentWidth;
-      y++;
-    }
-    
-  }
+  let largestarea = 0;
+  let pStack = [];
+  let hStack = [];
+  
+  // Each index point has a largest possible rectangle
+  // Iterate through heights
+    // Build stacks tracking heights and the index at which they start
+      // Add the height and position to the stacks if the new height is higher than the item at the top of the height stack
+    // When you reach a height that is lower than the top of the height stack you've reached the right-side boundary of a rectangle
+      // Pop the height off and calculate the area. 
+        // If larger than stored area, store new largest area
+      // If the next height is greater than current height- pop previous position off, pop that height off as well and calculate area
+      // If you reach a height that is less than the current height, store the current height
+    // When you reach the end of the heights array- start calculating heights of all remaining points in stacks
   return largestArea;
 };
+
+
+
+
+
+ 
+// var largestRectangleArea = function(heights) {
+//   let largestArea = 0;
+//   let storedDomains = [
+//     [],
+//   ];
+//   const scanRightAtHeight = (someHeight, someStartIndex) => {
+//     let domain = [];
+//     for (let x = someStartIndex; x < heights.length; x++) {
+//       if (someHeight <= heights[x]) {
+//         if (domain[0] === undefined) {
+//           domain = [x, x + 1];
+//         } else {
+//           domain[1] = x + 1;
+//         }
+//       } else {
+//         // console.log(`domain = ${JSON.stringify(domain)}`)
+//         return domain;
+//       }
+//     }
+//     console.log(`domain = ${JSON.stringify(domain)}`)
+//     return domain;
+//   }
+
+//   if (heights.length === 0) {
+//     return 0;
+//   }
+
+//   for (let x = 0; x < heights.length; x++) {
+//     let currentWidth = [];
+//     let previousWidth = [];
+//     let currentArea = 0;
+//     let y = 1;
+//     while (y <= heights[x]) {
+//       // If data at height is stored
+//       if (storedDomains[y]) {
+//         // Check to see if stored data at current height contains data at x
+//         for (let i = 0; i < storedDomains[y].length; i++) {
+//           // If data at height exists and x is in a stored domain
+//           if (storedDomains[y][i][0] <= x && storedDomains[y][i][1] >= x) {
+//             currentWidth = [storedDomains[y][i][0], storedDomains[y][i][1]]
+//           } else {
+//             // If data at height doesn't contain x, it's in a new domain; store it
+//             currentWidth = scanRightAtHeight(y, x);
+//             storedDomains[y].push(currentWidth);
+//           }
+//         }
+//       } else {
+//         // If no domains stored at height
+//         currentWidth = scanRightAtHeight(y, x);
+//         storedDomains[y] = [currentWidth];
+//       }
+//       // Calculate rectangle area at current x, for each height
+//       // If previousWidth empty
+//       if (previousWidth[0] !== undefined) {
+//         // If currentWidth[0] is left of previousWidth[0], replace
+//         currentWidth[0] = (currentWidth[0] < previousWidth[0]) ? previousWidth[0] : currentWidth[0];
+//         // If currentWidth[1] is right of previousWidth[1], replace
+//         currentWidth[1] = (currentWidth[1] > previousWidth[1]) ? previousWidth[1] : currentWidth[1];        
+//       }
+//       currentArea = (currentWidth[1] - currentWidth[0]) * y;
+//       largestArea = (largestArea > currentArea) ? largestArea : currentArea;
+//       previousWidth = currentWidth;
+//       y++;
+//     }
+    
+//   }
+//   return largestArea;
+// };
 
 largestRectangleArea([2,1,5,6,2,3]);
