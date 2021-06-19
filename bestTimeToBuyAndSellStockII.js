b4701a8002fc62bffcca866b28420f89a2500feb
Henry Yang <contacthenryyang@gmail.com>
2021-06-18 18:03:06 -0700

    Create file & pseudo

---
+++ bestTimeToBuyAndSellStockII.js
@@ -0,0 +1,57 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like
+(i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging
+multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e., max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 3 * 104
+0 <= prices[i] <= 104
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  // Capture all profit -making changes
+  // Starting at i = 1, check each index to see if it's greater than the previous
+  // Once you find an increase, track the starting point and keep iterating until you reach a decrease
+  // Add the profit to the profit tracker
+  let profit = 0;
+  let buy = undefined;
+  for (i = 1; i < prices.length; i++) {
+    if (!buy) { // No stock bought yet, looking to buy
+      if (prices[i] > prices[i - 1]) { // Found a price increase
+        buy = prices[i - 1];
+      }
+    } else { // Stock bought, looking to sell
+      
+    }
+  }
+};
