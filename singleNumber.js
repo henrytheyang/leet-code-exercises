e73ee7687f3b6cea3c3c42d3350e6e92e31a6c8b
Henry Yang <contacthenryyang@gmail.com>
2021-07-08 21:18:49 -0700

    Write first crack

---
+++ singleNumber.js
@@ -0,0 +1,45 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var singleNumber = function(nums) {
+  let answerBank = {};
+  for (i = 0; i < nums.length; i++) {
+    if (answerBank[nums[i] === undefined]) {
+      answerBank[nums[i]] = true;
+    } else {
+      delete answerBank[nums[i]];
+    }
+  }
+  return Object.keys(answerBank)[0];
+};
