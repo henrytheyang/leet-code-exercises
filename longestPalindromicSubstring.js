082ef515c99a5f5b08f1d16d45f6ee41f83124b2
Henry Yang <contacthenryyang@gmail.com>
2022-07-06 18:02:54 -0700

    Write out logic

--- longestPalindromicSubstring.js
+++ longestPalindromicSubstring.js
@@ -26,7 +26,42 @@ s consist of only digits and English letters.
  * @return {string}
  */
 var longestPalindrome = function(s) {
+  if (s.length === 0) {
+    return '';
+  }
+
+  if (s.length === 1) {
+    return s;
+  }
+
+  let answer = s[0];
+
   // Iterate through string
-  // Use middle out method to check for palindrome
-  // Run middle out again, checking for double letter in the center palindrome
+  for (i = 1; i < s.length; i++) {
+    // Use middle out method to check for palindrome
+    let left = i - 1;
+    let right = i + 1;
+    while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+      if (right - left + 1 > answer.length) {
+        answer = s.slice(left, right);
+      }
+    }
+
+    // If current string === next string, run middle out again, checking for double letter in the center palindrome
+    if (s[i] === s[i + 1]) {
+      let left = i - 1;
+      let right = i + 2;
+      if (answer.length < 2) { 
+        answer = s.slice(i, i + 1);
+      }
+      while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+        if (right - left + 1 > answer.length) {
+          answer = s.slice(left, right);
+        }
+      }
+    }
+
+  }
+
+  return answer;
 };
