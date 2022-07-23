a282de7632d3d4829cb91a751467d45d81e3a49c
Henry Yang <contacthenryyang@gmail.com>
2022-07-22 17:21:54 -0700

    Write code

--- firstMissingPositive.js
+++ firstMissingPositive.js
@@ -31,9 +31,31 @@ Constraints:
  */
 var firstMissingPositive = function(nums) {
   // Solution must be in the range 1...n + 1, where n is the length of the array
-  // Iterate through array and overwrite irrelevant numbers (negatives, numbers > n + 1)
+  // Iterate through array and overwrite irrelevant numbers (non-positives, numbers > n + 1)
+  const n = nums.length;
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] < 1 || nums[i] > n + 1) {
+      nums[i] = n + 1;
+    }
+  }
   // Iterate through array- take the value at each index, go to that index number, and mark it by making it negative
+  for (i = 0; i < nums.length; i++) {
+    let temp = Math.abs(nums[i])
+    if (temp > n) {
+      continue
+    }
+    // Subtract 1 from each number to match numbers to 0-index based array
+    temp--
+    if (nums[temp] > 0) {
+      nums[temp] = nums[temp] * (-1);
+    }
+  }
 
   // Iterate through array- return first positive number, else return n + 1
-
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      return i + 1;
+    }
+  }
+  return n + 1;
 };
