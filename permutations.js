269c7038f55ecd75d12f4dca12ae644aa3b959e8
Henry Yang <henrytheyang@gmail.com>
2022-08-12 15:15:53 -0700

    Adjust whitespace of comments

--- permutations.js
+++ permutations.js
@@ -1,5 +1,6 @@
 /*
-Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+Given an array nums of distinct integers, return all the possible permutations.
+You can return the answer in any order.
 
  
 
