8898b439af1c862d94cffdaf9d4fda486939cbda
Henry Yang <contacthenryyang@gmail.com>
2022-09-07 16:12:14 -0700

    Write code

--- minimumPathSum.js
+++ minimumPathSum.js
@@ -33,5 +33,26 @@ var minPathSum = function(grid) {
   // Create dp table recording minimum sum to arrive at each square
   // Each square is the sum of the squares above and to the left
   // Return last square
-  
+  let smallestPathHere = new Array(grid.length);
+  for (let i = 0; i < grid[0].length; i++) {
+    smallestPathHere[i] = new Array(grid[0].length);
+  }
+
+  smallestPathHere[0][0] = grid[0][0];
+  for (let j = 1; j < smallestPathHere[0].length; j++) {
+    smallestPathHere[0][j] = smallestPathHere[0][j - 1] + grid[0][j];
+  }
+  for (let k = 1; k < smallestPathHere.length; k++) {
+    smallestPathHere[k][0] = smallestPathHere[k - 1][0] + grid[k][0];
+  }
+
+  for (let l = 1; l < smallestPathHere.length; l++) {
+    for (let m = 1; m < smallestPathHere[0].length; m++) {
+      smallestPathHere[l][m] = Math.min(smallestPathHere[l - 1][m], smallestPathHere[l][m - 1]) + grid[l][m];
+    }
+  }
+
+  return smallestPathHere[grid.length - 1][grid[0].length - 1];
 };
+
+minPathSum([[1,2,3],[4,5,6]])
