5c7350e2e9e0aae9c7e81eca3eb9eeea2a342761
Henry Yang <contacthenryyang@gmail.com>
2022-09-25 14:16:41 -0700

    Write pseudo for new strat

--- 4sum.js
+++ 4sum.js
@@ -31,41 +31,10 @@ Constraints:
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
-  // Nested two pointer solution
-  let min = 0;
-  let max = nums.length - 1;
-  let answer = [];
-  let numsSorted = nums.sort((a, b) => a - b);
+  // Previous double set of two pointer solution missed too many solutions by only incrementing 
+    // outside left pointer when a solution was found
+  // Need to do a 3sum solution nested inside a for loop to find all solutions
   
-  while (min + 3 <= max) {
-    let cycleBest = Infinity;
-    let left = min + 1;
-    let right = max - 1;
-    
-    while (left < right) {
-      let sum = numsSorted[min] + numsSorted[left] + numsSorted[right] + numsSorted[max];
-      if (Math.abs(target - sum) < Math.abs(target - cycleBest)) cycleBest = sum;
-      if (sum === target) answer.push([numsSorted[min], numsSorted[left], numsSorted[right], numsSorted[max]]);
-      if (sum <= target) {
-        left++;
-        while (numsSorted[left] === numsSorted[left - 1]) left++;
-      }
-      else if (sum > target) {
-        right--;
-        while (numsSorted[right] === numsSorted[right + 1]) right--;
-      };
-    }
-
-    if (cycleBest <= target) {
-      min++
-      while (numsSorted[min] === numsSorted[min - 1]) min++;
-    }
-    else if (cycleBest > target) {
-      max--
-      while (numsSorted[max] === numsSorted[max + 1]) max--;
-    };
-  }
-  return answer;
 };
 fourSum([-3,-2,-1,0,0,1,2,3], 0)
 
