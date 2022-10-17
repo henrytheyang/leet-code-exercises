b029a36bcad6a1caf940e1266bb0e8a6ae65f1ab
Henry Yang <contacthenryyang@gmail.com>
2022-10-17 16:26:47 -0700

    Write code

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -35,4 +35,27 @@ var groupAnagrams = function(strs) {
   // Compare sorted string to all encountered archetype
     // If there's a match, add it to the matched answer array
     // If there's no match, make a new answer array and add to archetype
+  let answer = [];
+  let archetypes = {};
+  let sorted = [...strs];
+  for (let i = 0; i < sorted.length; i++) {
+    sorted[i] = [...sorted[i]].sort().join('');
+  }
+
+  for (let j = 0; j < sorted.length; j++) {
+    if (sorted[j] in archetypes === false) {
+      archetypes[sorted[j]] = {answerIndex: answer.length};
+      answer.push([strs[j]])
+    } else {
+      answer[archetypes[sorted[j]].answerIndex].push(strs[j]);
+    }
+  }
+
+  return answer;
 };
+groupAnagrams(["eat","tea","tan","ate","nat","bat"]);
+
+/*
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+*/
