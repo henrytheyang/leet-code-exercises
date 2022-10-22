b51ecd36fb22d295925e283f058b096cdfded3f5
Henry Yang <contacthenryyang@gmail.com>
2022-10-21 17:18:47 -0700

    Write code

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -30,5 +30,36 @@ var longestConsecutive = function(nums) {
   // Scan contents of nums into an object
   // For... in loop the contents of object; numbers are iterated in ascending order ES6
   // Then iterate through and track longest streak
+  let answer = 0;
+  let currentStreak = 1;
+  let obj = {};
+  let ordered = [];
 
+  if (nums.length <= 1) return nums.length;
+  let lastNumber = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    obj[nums[i]] = nums[i];
+  }
+  for (props in obj) {
+    ordered.push(props);
+  }
+  for (let j = 1; j < ordered.length - 1; j++) {
+    if (ordered[j] === lastNumber + 1) currentStreak++;
+    else currentStreak = 1;
+
+    lastNumber = ordered[j];
+    if (currentStreak > answer) answer = currentStreak;
+  }
+
+  return answer;
 };
+longestConsecutive([100,4,200,1,3,2])
+/*
+Your input
+[100,4,200,1,3,2]
+Output
+1
+Expected
+4
+*/
