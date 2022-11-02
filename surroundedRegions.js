107a2940a2d169e503c904aab901316fe32f6e92
Henry Yang <contacthenryyang@gmail.com>
2022-11-01 21:40:21 -0700

    Write new pseudo strat

--- surroundedRegions.js
+++ surroundedRegions.js
@@ -33,71 +33,43 @@ board[i][j] is 'X' or 'O'.
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
-  // When we find first O start tracking current streak
-    // If we reach edge of board without enclosing, delete streak
-    // If we encounter another O in the row below it, it's part of the current streak, keep checking
-    // If we ever reach an edge with an O and it's not enclosed, delete current streak
-  let queuePossible = [];
-  let confirmedFlips = [];
-  let seen = new Array(board.length).fill(0).map(x => new Array(board[0].length).fill(false));
+  // Check for illegal 'O' on border, and mark each, and each 'O' connected to it
+  // Flip every other 'O'
   
-  const checkPeripheralO = (m, n) => {
-    // If we encounter O on invalid edge return O
-    // Else return all Os to check
-    let detected = [];
-    if (board[m - 1][n] === 'O') {
-      if (m - 1 === 0) {
-        return false
-      } else detected.push({m: m - 1, n: n});
-    } 
-    if (board[m + 1][n] === 'O') {
-      if (m + 1 === board.length - 1) {
-        return false
-      } else detected.push({m: m + 1, n: n});
-    } 
-    if (board[m][n - 1] === 'O') {
-      if (n - 1 === 0) {
-        return false
-      } else detected.push({m: m, n: n - 1})
-    }
-    if (board[m][n + 1] === 'O') {
-      if (n + 1 === board[0].length - 1) {
-        return false
-      } else detected.push({m: m, n: n + 1});
-    }
-    // only here if we pass all
-    queuePossible = [...queuePossible, ... detected]
-    return true;
-  }
-
-  for (let i = 1; i < board.length - 1; i++) {
-    for (let j = 1; j < board[0].length - 1; j++) {
-      if (seen[i][j] === true) continue;
-
-      seen[i][j] = true;
-      if (board[i][j] === 'O') {
-        queuePossible.push({m: i, n: j});
-        if (checkPeripheralO(i, j) === true) {
-          let counter = 0;
-          while (queuePossible.length > counter) {
-            let currentCoord = queuePossible[counter];
-            counter++;
-            seen[currentCoord.m][currentCoord.n] = true;
-            if (checkPeripheralO(currentCoord.m, currentCoord.n) === false) {
-              queuePossible = [];
-            }
-          }
-          if (queuePossible.length > 0) {
-            confirmedFlips = [...confirmedFlips, ...queuePossible];
-            queuePossible = [];
-          }
-        }
-      }
-    }
-  }
-  for (let k = 0; k < confirmedFlips.length; k++) {
-    board[confirmedFlips[k].m][confirmedFlips[k].n] = 'X';
-  }
 };
 
-solve([["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]])
+solve([
+  ["O","O","O","O","X","X"],
+  ["O","O","O","O","O","O"],
+  ["O","X","O","X","O","O"],
+  ["O","X","O","O","X","O"],
+  ["O","X","O","X","O","O"],
+  ["O","X","O","O","O","O"]
+])
+
+/*
+Input
+[
+  ["O","O","O","O","X","X"],
+  ["O","O","O","O","O","O"],
+  ["O","X","O","X","O","O"],
+  ["O","X","O","O","X","O"],
+  ["O","X","O","X","O","O"],
+  ["O","X","O","O","O","O"]]
+Output
+[
+  ["O","O","O","O","X","X"],
+  ["O","O","O","O","O","O"],
+  ["O","X","O","X","O","O"],
+  ["O","X","O","X","X","O"],
+  ["O","X","O","X","O","O"],
+  ["O","X","O","O","O","O"]]
+Expected
+[
+  ["O","O","O","O","X","X"],
+  ["O","O","O","O","O","O"],
+  ["O","X","O","X","O","O"],
+  ["O","X","O","O","X","O"],
+  ["O","X","O","X","O","O"],
+  ["O","X","O","O","O","O"]]
+*/
