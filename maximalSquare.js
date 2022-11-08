16f4a2a7dcc2bd85803575a1cdf917102c97bcec
Henry Yang <contacthenryyang@gmail.com>
2022-11-07 22:55:55 -0800

    Improve speed by 50% space to 93 percentile

--- maximalSquare.js
+++ maximalSquare.js
@@ -31,7 +31,45 @@ matrix[i][j] is '0' or '1'.
  * @param {character[][]} matrix
  * @return {number}
  */
+
+
 var maximalSquare = function(matrix) {
+  // Create dp square tracking how many rows deep the 1s are
+  // If a dp row contains as many columns as the rows deep of 1s, that's a square
+  // Track the current max squares
+
+  // Space consideration- O(m * n)
+    // Since we only need the info from the prev row, we only need to save one row at a time.
+    // We can reduce space complexity to O(n)
+
+  // Time complexity- O(m * n * k), where k is avg length our loop backtracks to look for new square
+    // We can decrease some linear amount of time by making our code fill in dp and check for new area as we go
+  let length = 0;
+  let dp = new Array(matrix[0].length);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (i === 0) dp[j] = matrix[i][j] * 1;
+      else if (matrix[i][j] ===  '0') dp[j] = 0;
+      else {
+        dp[j] = dp[j] === 0 ? 1 : dp[j] + 1;
+      }
+
+      let minHeight = dp[j];
+      let counter = 0;
+      for (let k = j; k >= 0; k--) {
+        if (dp[k] === 0) break;
+        if (dp[k] < minHeight) minHeight = dp[k];
+        counter = counter === 0 ? 1 : counter + 1;
+        if (minHeight > length && counter > length) length = Math.min(counter, minHeight);
+      }
+    }
+  }
+  return length * length;
+}
+maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
+
+
+var maximalSquareBigSpace = function(matrix) {
   // Create dp square tracking how many rows deep the 1s are
   // If a dp row contains as many columns as the rows deep of 1s, that's a square
   // Track the current max squares
@@ -70,8 +108,6 @@ var maximalSquare = function(matrix) {
   }
   return length * length;
 };
-
-maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
 /*
 Your input
 [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
