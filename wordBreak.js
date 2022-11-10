71d3b95e82a9dbecd476d08b88ae8777a0884862
Henry Yang <contacthenryyang@gmail.com>
2022-11-09 21:24:34 -0800

    Write code

--- wordBreak.js
+++ wordBreak.js
@@ -35,6 +35,44 @@ All the strings of wordDict are unique.
  * @param {string[]} wordDict
  * @return {boolean}
  */
+
 var wordBreak = function(s, wordDict) {
-    
+  // Create dp array that tracks if it's possible to reach the end of the string from the current index
+  // dp[i] is true if
+    // it can reach the end of the string with a word from the dictionary
+    // it can reach dp[someIndex], where dp[someIndex] is true
+  // If we can make it to dp[0] and dp[0] is true, return true; else return false
+  let dp = new Array(s.length + 1).fill(false);
+  dp[s.length] = true;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+
+    for (let j = 0; j < wordDict.length; j++) {
+      let indexTrue = false;
+      // Iterate through wordDict, checking if word can reach either end of string or true dp[i];
+      // Skip to next word if word length and index don't add to a true index
+      if (dp[wordDict[j].length + i] !== true || wordDict[j].length + i >= s.length) continue;
+      // Check if currrent word starting on current index can end on a true dp[someIndex]
+      for (let k = 0; k < wordDict[j].length; k++) {
+        if (wordDict[j][k] !== s[i + k]) break;
+        if (k = wordDict[j].length - 1) indexTrue = true;
+      }
+      if (indexTrue) {
+        dp[i] = true
+        break;
+      }
+    }
+  }
+  if (dp[0]) return true;
+  else return false;
 };
+wordBreak('leetcode', ["leet","code"])
+/*
+Your input
+"leetcode"
+["leet","code"]
+Output
+false
+Expected
+true
+*/
