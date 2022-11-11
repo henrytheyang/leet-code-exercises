cc07f332bbdc928444da795b8dc2aff9b7196ef5
Henry Yang <contacthenryyang@gmail.com>
2022-11-11 15:08:46 -0800

    Write code

--- longestIncreasingSubsequence.js
+++ longestIncreasingSubsequence.js
@@ -35,4 +35,22 @@ var lengthOfLIS = function(nums) {
   // DP Solution
   // Starting from the rear, track how long of an incrementing sequence we can make from the current index
     // For each index, it's max of (1, or 1 + any dp[index]) that is larger than the current value
+  let dp = new Array(nums.length).fill(1);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      if (nums[i] < nums[j]) dp[i] = Math.max(dp[i], 1 + dp[j]);
+    }
+  }
+  
+  return dp[0];
 };
+lengthOfLIS([10,9,2,5,3,7,101,18]);
+
+/*
+Your input
+[10,9,2,5,3,7,101,18]
+Output
+2
+Expected
+4
+*/
