5b84b9ef89dc308ab33e3f8cbc21faa10d554aa4
Henry Yang <contacthenryyang@gmail.com>
2022-11-18 19:51:00 -0800

    Write code

--- sortAnArray.js
+++ sortAnArray.js
@@ -29,15 +29,41 @@ Constraints:
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
-  // Divide & conquer strat + 3 pointers (pivot, i,j)
-    // Left & right side of array, set pivot to right side
-    // Increment j from left to pivot
-    // When you hit a value that is less than the pivot value
-      // Increment i, swap positions of i and j
-    // Keep incrementing j until it equals p
-    // Increment i one last time, swap places of p and i
-  // Next round, recursively run pivot on the two subarrays you've made on either side of pivot
-  // Continue while the subarrays left & right edges are discreet
+  var sortArray = function(nums, left = 0, right = nums.length - 1) {
+    // Divide & conquer strat + 3 pointers (pivot, i,j)
+      // Left & right side of array, set pivot to right side
+      // Increment j from left to pivot
+      // When you hit a value that is less than the pivot value
+        // Increment i, swap positions of i and j
+      // Keep incrementing j until it equals p
+      // Increment i one last time, swap places of p and i
+    // Next round, recursively run pivot on the two subarrays you've made on either side of pivot
+    // Continue while the subarrays left & right edges are discreet
+    const pivot = (leftIndex, rightIndex) => {
+      let i = leftIndex - 1;
+      let pivot = rightIndex;
+      for (let j = leftIndex; j < pivot; j++) {
+        if (nums[j] < nums[pivot]) {
+          i++;
+          let temp = nums[i];
+          nums[i] = nums[j];
+          nums[j] = temp;
+        }
+      }
+      i++;
+      let temp = nums[i];
+      nums[i] = nums[pivot];
+      nums[pivot] = temp;
 
-};
+      return i;
+    }
+
+    if (left < right) {
+      let pivotIndex = pivot(left, right);
+      sortArray(nums, left, pivotIndex - 1);
+      sortArray(nums, pivotIndex + 1, right);
+    }
+    return nums;
+  };
+
+  sortArray([5,2,3,1]);
