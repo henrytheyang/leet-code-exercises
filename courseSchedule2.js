bec1735e911e1625cb80ce3e99899e9e3ff31723
Henry Yang <contacthenryyang@gmail.com>
2022-12-05 23:46:08 -0800

    Write code

--- courseSchedule2.js
+++ courseSchedule2.js
@@ -40,4 +40,47 @@ var findOrder = function(numCourses, prerequisites) {
   // Track for double visits; if we encounter loop break and return []
   // If we reach a node that has 0 prereqs, add to answer order, remember that it's used
   // Return up a level, pop that prereq off the list, remove visited tag
+  let answer = [];
+  let addedToAnswer = new Array(numCourses).fill(false);
+  let loopDetector = [...addedToAnswer];
+  let graph = new Array(numCourses).fill(0).map(element => new Array);
+  for (let i = 0; i < prerequisites.length; i++) {
+    graph[prerequisites[i][0]].push(prerequisites[i][1]);
+  }
+  const dfs = (node) => {
+    if (loopDetector[node] === true) return false; // order?
+    if (addedToAnswer[node] === true) return true;
+
+    loopDetector[node] = true;
+    // Iterate through list of dependencies if they exist
+    for (let j = graph[node].length - 1; j >= 0; j--) {
+      // let nextNode = graph[node][j];
+      let nextNode = graph[node].pop();
+      if (dfs(nextNode) === false) return answer;
+    }
+    loopDetector[node] = false;
+    if (graph[node].length === 0) { // Detect end of depenedency line
+      answer.push(node);
+      addedToAnswer[node] = true;
+    }
+  }
+
+  for (let k = 0; k < numCourses; k++) {
+    if (dfs(k) === false) return answer;
+  }
+
+  return answer;
 };
+findOrder(2, [[0,1],[1,0]]);
+
+/*
+Input:
+2
+prerequisites =
+[[0,1],[1,0]]
+22 / 45 testcases passed
+Output
+[0]
+Expected
+[]
+*/
