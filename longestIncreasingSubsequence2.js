b3f62397b6b993e2076e5413950a1de29838ec57
Henry Yang <contacthenryyang@gmail.com>
2023-01-12 00:11:44 -0800

    Fix binary search

--- longestIncreasingSubsequence2.js
+++ longestIncreasingSubsequence2.js
@@ -63,14 +63,14 @@ var lengthOfLIS = function(nums, k) {
     let low = 0;
     let high = indexOfMinLastValOfSeq.length - 1;
     let mid = Math.floor((low + high) / 2); // Looking for mid, the first val greater than our searched val
-    while (nums[indexOfMinLastValOfSeq[mid]] < val || nums[indexOfMinLastValOfSeq[mid - 1]] > val) {
+    while (low < high) {
       if (nums[indexOfMinLastValOfSeq[mid]] === val) return false;
       else if (nums[indexOfMinLastValOfSeq[mid]] < val) {
         low = mid + 1;
         mid = Math.floor((low + high) / 2);
       }
-      else if (nums[indexOfMinLastValOfSeq[mid - 1]] && nums[indexOfMinLastValOfSeq[mid - 1]] > val) {
-        high = mid - 1;
+      else if (nums[indexOfMinLastValOfSeq[mid]] > val) {
+        high = mid;
         mid = Math.floor((low + high) / 2);
       }
     }
@@ -79,7 +79,10 @@ var lengthOfLIS = function(nums, k) {
   }
 
   for (let i = 0; i < nums.length; i ++) {
-    if (indexOfMinLastValOfSeq.length === 0 || nums[i] < nums[indexOfMinLastValOfSeq[0]]) {
+    // if (i === 45) {
+    //   console.log('here');
+    // }
+  if (indexOfMinLastValOfSeq.length === 0 || nums[i] < nums[indexOfMinLastValOfSeq[0]]) {
       indexOfMinLastValOfSeq[0] = i;
     } else if (nums[i] > nums[indexOfMinLastValOfSeq[indexOfMinLastValOfSeq.length -1]]) {
       indexOfMinLastValOfSeq[indexOfMinLastValOfSeq.length] = i;
@@ -99,7 +102,7 @@ var lengthOfLIS = function(nums, k) {
   return answer.length;
 };
 
-lengthOfLIS([4,2,1,4,3,4,5,8,15], 3)
+lengthOfLIS([1,3,6,7,9,4,10,5,6]);
 /*
 Input
 nums =
@@ -111,3 +114,5 @@ NaN
 Expected
 5
 */
+
+[-147,-171,-584,590,501,13,489,-938,396,-544,-229,697,157,-933,-264,-436,-691,-469,49,-574,694,50,672,-858,-923,974,-157,-507,-907,429,529,-591,802,-351,-606,296,-485,454,540,300,-1000,408,923,0,-975,-548,62,-990,835,650,733,-611,-385,-580,330,394,566,-191,612,-608,-478,-104,-425,58,-849,601,851,-208,-810,400,412,571,-535,-995,627,-481,-702,457,-29,375,792,-186,-921,-275,654,-356,-322,-28,-843,527,-266,-970,556,852,-890,169,-413,2,-958,-651,371,895,-994,671,243,-605,-556,735,-246,179,-104,-771,658,-554,932,-829,-455,-981,-731,-148,512,-547,-946,-997,-197,864,870,629,-961,659,574,543,-501,582,-799,-428,876,-334,115,759,197,-905,275,76,242,357,694,-254,-361,-338,-57,596,786,-710,-51,-496,-100,246,-969,874,504,938,931,-365,175,-40,-616,596,440,567,999,15,-363,-256,-578,-869,-653,78,-352,882,749,-33,462,-592,-751,761,-96,206,489,34,367,960,68,837,37,-764,-897,72,639,-69,353,836,-67,491,126,-171,-532,-757,-358,217,806,712,-32,843,-790,-691,-381,-138,6,-712,153,-184,-544,3,840,-561,917,-704,-126,-230,468,963,-993,445,-892,-543,941,-665,58,268,-362,181,-529,684,313,-380,-712,700,601,-962,-886,702,439,153,-87,140,583,-323,70,-460,-863,-859,-784,571,169,44,-460,181,883,600,982,-367,-191,815,-84,961,-791,-713,149,-499,330,-351,-442,-989,-662,-183,-220,-617,-638,-916,454,604,559,-304,-812,526,-891,984,-762,-669,-414,-481,-219,-776,690,-72,-250,-282,-961]
