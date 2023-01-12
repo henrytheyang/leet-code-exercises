2adbaaf528b3186cff2b41f290b93a7e19cb1e8f
Henry Yang <contacthenryyang@gmail.com>
2023-01-12 00:09:34 -0800

    Optimized n^2 time sol to nlogn, 3x speed

--- longestIncreasingSubsequence.js
+++ longestIncreasingSubsequence.js
@@ -36,7 +36,61 @@ Follow up: Can you come up with an algorithm that runs in O(n log(n)) time compl
 // Whenever we encounter a new value we either add it to the end of the array if the value
 // is larger than the largest value, or we replace the first value that's larger than it
 
+var lengthOfLIS = function(nums, k) {
+  // DP solution, monotonic queue solution
+  // We can eliminate extraneous values from nums by only tracking the index of the minimum last
+  // value of an increasing subsequence of length x
+  // For each value stored, we can store its path in an array by pointing at the index before it
+  // Increment through nums
+    // If the value is smaller than the first value stored, replace the first index
+    // If the value is greater than the last value stored, add it to the end of the array storing lasts
+    // Else use binary search to find the stored value larger than the current value, and replace it
+    // Store the value that comes immediately comes before it in the path
+  let path = new Array(nums.length).fill(-1);
+  let indexOfMinLastValOfSeq = new Array();
+  let answer = [];
+  const replaceValueBinarySearch = (val, index) => {
+    let low = 0;
+    let high = indexOfMinLastValOfSeq.length - 1;
+    let mid = Math.floor((low + high) / 2); // Looking for mid, the first val greater than our searched val
+    while (low < high) {
+      if (nums[indexOfMinLastValOfSeq[mid]] === val) return false;
+      else if (nums[indexOfMinLastValOfSeq[mid]] < val) {
+        low = mid + 1;
+        mid = Math.floor((low + high) / 2);
+      }
+      else if (nums[indexOfMinLastValOfSeq[mid]] > val) {
+        high = mid;
+        mid = Math.floor((low + high) / 2);
+      }
+    }
+    indexOfMinLastValOfSeq[mid] = index;
+    path[index] = indexOfMinLastValOfSeq[mid - 1];
+  }
 
+  for (let i = 0; i < nums.length; i ++) {
+    // if (i === 45) {
+    //   console.log('here');
+    // }
+  if (indexOfMinLastValOfSeq.length === 0 || nums[i] < nums[indexOfMinLastValOfSeq[0]]) {
+      indexOfMinLastValOfSeq[0] = i;
+    } else if (nums[i] > nums[indexOfMinLastValOfSeq[indexOfMinLastValOfSeq.length -1]]) {
+      indexOfMinLastValOfSeq[indexOfMinLastValOfSeq.length] = i;
+      path[i] = indexOfMinLastValOfSeq[indexOfMinLastValOfSeq.length - 2];
+    } else {
+      replaceValueBinarySearch(nums[i], i);
+    }
+  }
+
+  let counter = indexOfMinLastValOfSeq.length - 1;
+  let nextIndex = indexOfMinLastValOfSeq[counter];
+  while (counter >= 0) {
+    answer[counter] = nums[nextIndex];
+    nextIndex = path[nextIndex];
+    counter--;
+  }
+  return answer.length;
+};
 
 
 var DPOnlyLengthOfLIS = function(nums) {
