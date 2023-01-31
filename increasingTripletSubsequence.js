1d264b8de9f8b9358931be302ce5bfb77fb54df5
Henry Yang <contacthenryyang@gmail.com>
2023-01-30 22:08:16 -0800

    Finish nested loop solution

--- increasingTripletSubsequence.js
+++ increasingTripletSubsequence.js
@@ -35,5 +35,32 @@ Follow up: Could you implement a solution that runs in O(n) time complexity and 
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-    
+  // Greedy solution from rear, O(n^2) solution
+  // Start from rear, nested loops to check 
+  if (nums.length < 3) return false;
+  let dp = new Array(nums.length).fill(1);
+  let streak = 1;
+  for (let i = nums.length - 2; i >= 0; i--) {
+    streak = 1;
+    for (let j = i + 1; j <= nums.length - 1; j++) {
+      if (nums[i] < nums[j]) {
+        streak = Math.max(streak, dp[j] + 1);
+        if (streak === 3) return true;
+      }
+    }
+    dp[i] = streak;
+  }
+  return false;
 };
+
+increasingTriplet([6,7,1,2]);
+/*
+Input
+nums =
+[6,7,1,2]
+70 / 77 testcases passed
+Output
+true
+Expected
+false
+*/
